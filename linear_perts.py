fixed cw vs acw rotation, fixed kep v bug

linear_perts.py
@@ -56,8 +56,14 @@ class LinearPerts():
 
         # scale to cgs units (and account for rotation direction)
         self.pert_v_r_sq = cut_v_r * self.p.c_s_planet*(self.p.m_planet/self.p.m_thermal)
-        self.pert_v_phi_sq = cut_v_phi * self.p.c_s_planet*(self.p.m_planet/self.p.m_thermal)*self.p.a_cw
+        self.pert_v_phi_sq = cut_v_phi * self.p.c_s_planet*(self.p.m_planet/self.p.m_thermal)
         self.pert_rho_sq  = cut_rho * (self.p.m_planet/self.p.m_thermal)
+
+        # account for rotation direction
+        if self.p.a_cw == -1:
+            self.pert_v_r_sq = np.flipud(self.pert_v_r_sq)
+            self.pert_v_phi_sq = -1*np.flipud(self.pert_v_phi_sq)
+            self.pert_rho_sq = np.flipud(self.pert_rho_sq)
 
         """
         # plotting (for debugging)

plotting.py
@@ -22,15 +22,20 @@ def midplane_comparison(phantom_dump, analytics_file_location):
     A_vphi = np.transpose(np.load(analytics_file_location + '/vphi.npy'))
     A_rho = np.transpose(np.load(analytics_file_location + '/density.npy'))
 
+    # get grid for analytics
+    A_x = np.linspace(-500, 500, 1000)
+    A_y = np.linspace(-500, 500, 1000)
+    A_X, A_Y = np.meshgrid(A_x, A_y)
+
+    #print(A_rho.shape)
+
+    # applying exponential taper
+    #A_rho *= np.exp(-np.sqrt(A_X**2 + A_Y**2)/100)
+    
     # scale density for analytics
     A_rho *= np.median(PD.rho_xy) / np.median(A_rho)
 
     # ======= INTERPOLATION TO SAME GRID AND RESIDUALS ======= #
-
-    # get grid for analytics
-    A_x = np.linspace(-500, 500, 1000)
-    A_y = np.linspace(-500, 500, 1000)
-    #A_X, A_Y = np.meshgrid(A_x, A_y)
 
     # interpolating functions for analytics
     interp_v_r = RectBivariateSpline(A_y, A_x, A_vr)
@@ -43,15 +48,15 @@ def midplane_comparison(phantom_dump, analytics_file_location):
     new_X, new_Y = np.meshgrid(new_x, new_y)
 
     # evaluate interpolation on Grid object grid
-    A_int_vr = interp_v_r.ev(new_Y, new_X) + 1E-8
+    A_int_vr = interp_v_r.ev(new_Y, new_X)
     A_int_vphi = interp_v_phi.ev(new_Y, new_X)
     A_int_rho = interp_v_rho.ev(new_Y, new_X)
 
     # find residuals
     
-    R_vr = PD.vr_xy - A_int_vr
-    R_vphi = np.abs(A_int_vphi - PD.vphi_xy)
-    R_rho = np.abs(A_int_rho - PD.rho_xy)
+    R_vr = -1*(PD.vr_xy - A_int_vr)
+    R_vphi = -1*(-1*PD.vphi_xy - -1*A_int_vphi )
+    R_rho = np.abs(PD.rho_xy - A_int_rho)
     
     """
     R_vr = np.abs((PD.vr_xy) - A_int_vr)
@@ -80,7 +85,7 @@ def midplane_comparison(phantom_dump, analytics_file_location):
 
     # colourbars
     fig.colorbar(pcm_vr, ax=ax[0, :], location='right', label='Radial Velocity [km/s]')
-    fig.colorbar(pcm_vphi, ax=ax[1, :], location='right', label='Azimutal Velocity [km/s]')
+    fig.colorbar(pcm_vphi, ax=ax[1, :], location='right', label='Azimuthal Velocity [km/s]')
     fig.colorbar(pcm_rho, ax=ax[2, :], location='right', label='Density [g/cm$^3$]')
 
     # labels
@@ -93,7 +98,7 @@ def midplane_comparison(phantom_dump, analytics_file_location):
     # titles
     ax[0,0].set_title('Analytical Model')
     ax[0,1].set_title('Simulated Model')
-    ax[0,2].set_title('Residuals')
+    ax[0,2].set_title('Analytics - Simulation')
 
     plt.savefig('midplane_comparisons.pdf')
     #plt.show()

setup.py
@@ -132,8 +132,8 @@ class Parameters(Constants):
         self.l = (2/3) * self.hr_planet * self.r_planet
 
         # get sound speed at planet radius and reference radius
-        v_kep_planet = np.sqrt(self.G_const*self.m_planet*self.m_solar / (self.r_planet*self.au))
-        v_kep_ref = np.sqrt(self.G_const*self.m_planet*self.m_solar / (self.r_ref*self.au))
+        v_kep_planet = np.sqrt(self.G_const*self.m_star*self.m_solar / (self.r_planet*self.au))
+        v_kep_ref = np.sqrt(self.G_const*self.m_star*self.m_solar / (self.r_ref*self.au))
         self.c_s_planet = v_kep_planet*self.hr_planet
         self.c_s_0 = v_kep_ref*self.hr 
 