fae05f50a15777ced050b222032f16dbc51af34c
TomHilder <tom.hilder.dlhp@gmail.com>
2021-09-30 14:37:36 +1000

    starting spiral line of sight and observation channel maps hybrid plots

---
+++ spiral.py
@@ -0,0 +1,19 @@
+### Import Libraries
+
+import  numpy               as      np
+import  matplotlib.pyplot   as      plt
+import  pymcfost            as      pmc
+
+from    matplotlib.colors   import  LogNorm
+from    matplotlib          import  ticker, cm
+
+
+### Plotting parameters
+
+# which channels to plot
+# which wakeflow model to use (probably don't generate a new one)
+
+# which observations to use
+
+
+### 
