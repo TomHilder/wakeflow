edf397920589d30cfbdf4379ae21f3476be27631
Tom Hilder <tom.hilder.dlhp@gmail.com>
2022-08-25 19:12:49 +1000

    bump version 1.1.0 -> 1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Wakeflow'
 copyright = '2022, Thomas Hilder'
 author = 'Thomas Hilder'
-release = '1.1.0'
+release = '1.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- src/wakeflow/__init__.py
+++ src/wakeflow/__init__.py
@@ -6,7 +6,7 @@ Wakeflow allows users to generate and manipulate semi-analytic models of planet 
 """
 
 # wakeflow package versionss
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 # give users access to the WakeflowModel class
 from .wakeflow import WakeflowModel
