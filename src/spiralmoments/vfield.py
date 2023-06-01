8ad8e16914b8c3e174ace7bf9be240f7f11c60c1
Tom Hilder <tom.hilder.dlhp@gmail.com>
2023-06-01 15:51:29 +1000

    added VelocityField class, not yet implemented

---
+++ src/spiralmoments/vfield.py
@@ -0,0 +1,11 @@
+# vfield.py
+# Written by Thomas Hilder
+
+"""
+TODO : Add code to rotate and project velocity fields.
+"""
+
+class VelocityField:
+    
+    def __init__(self) -> None:
+        raise NotImplementedError
